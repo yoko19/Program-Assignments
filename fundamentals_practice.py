18cfe4a782d10460bc23293da708db93e5585266
yoko19 <103329202+yoko19@users.noreply.github.com>
2022-04-12 18:38:52 -0700

    fundamentals practice

---
+++ fundamentals_practice.py
@@ -0,0 +1,18 @@
+print ("hello world")    #hello world
+name = "David"
+age = 17
+years_at_hth = 0.1
+plays_basketball = False
+jersey_number = "n/a"
+car_name = "Acura", "honda", "toyota", "nissan", "subaru", 
+x = 50 
+first_name = "David" 
+print(name)
+print(age)
+print("years")
+print(years_at_hth)
+print(plays_basketball)
+print("jesey_number")
+print(car_name)
+print(x)
+print(first_name)
